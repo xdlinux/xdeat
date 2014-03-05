f88dcc0fb53408c4ac1bf32489da663db67570e4
kok liu <kokdemo@gmail.com>
2014-03-05 16:40:57 +0800

    fix the mobile page

--- js/random.js
+++ js/random.js
@@ -28,8 +28,23 @@ function rand(itsid){
 		GetRandomn=Math.floor(Math.random()*n+1)
 	};
 	GetRandomFunc(chooses.length);
-	$('#answer').html("第"+n+"次随机的答案:"+"<font size='6' color='orange'>"+chooses[GetRandomn-1]+"</font>");
+	if (n<10) {
+		$('#answer').html("第"+n+"次随机的答案:"+"<font size='6' color='orange'>"+chooses[GetRandomn-1]+"</font>");}
+	else{
+		if (n>20) {
+			$('#answer').html("喂，这已经"+n+"次了，这次是"+"<font size='6' color='yellow'>"+chooses[GetRandomn-1]+"</font>"+"，你不饿吗？");
+		} 
+		else{
+			$('#answer').html("你已经随机了"+n+"次了，这次是"+"<font size='6' color='red'>"+chooses[GetRandomn-1]+"</font>");
+		};
+	};
+	
 	$('#title').html("试试"+chooses[GetRandomn-1]);
-	$("#randtime").html("这是第"+n+"次随机");
+	if (n<10) {
+		$("#randtime").html("这是第"+n+"次随机");
+	}
+	else{
+		$("#randtime").html("这已经是第"+n+"次随机了！您其实不饿对吧！");
+	};
 	n=n+1;
 }
